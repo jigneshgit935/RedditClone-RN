e8a4d5c5cdecfe79a9c61ca3d6f29f0ed51dec66
Jignesh <bhairavasoft@gmail.com>
2025-07-20 16:54:46 +0530

    project-setup with bottomtab

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,64 @@
+import { Tabs } from "expo-router";
+import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
+
+export default function TabLayouts() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "black" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Reddit",
+          headerTintColor: "#ff5700",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="communities"
+        options={{
+          title: "Communities",
+          headerTintColor: "#ff5700",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerTintColor: "#ff5700",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="plus" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chats"
+        options={{
+          title: "Chat",
+          headerTintColor: "#ff5700",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="chatbox-ellipses-outline" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          headerTintColor: "#ff5700",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color }) => (
+            <Feather name="bell" color={color} size={24} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/(tabs)/chats.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function ChatsScreen() {
+  return <Text>ChatsScreen</Text>;
+}
